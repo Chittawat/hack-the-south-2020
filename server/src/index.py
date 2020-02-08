e376015f8aac2306fc5230671699e90717010280
cp5g15 <chittawat.pourpongpan@tesco.com>
2020-02-08 19:51:08 +0000

    fix frontend

--- server/src/index.py
+++ server/src/index.py
@@ -3,17 +3,26 @@ Server File
 - Handles Entering and Exiting of the Backend
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from services import get_news
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+	return "hello World"
 
-@app.route('/info')
+@app.route('/info/<country>')
 def news(country):
-    news_data = get_news(country)
-    return render.template('../../webpage/news.html', posts=news_data)
-
+	location=country
+	news_data = get_news(str(location))
+	news=[]
+	for i in news_data:
+		newsinfo={'source':i[0],'title':i[1],'link':i[2],'detail':i[3]}
+		news.append(newsinfo)
+	return render_template('news.html', post=news)
+	
+if __name__ == '__main__':
+	app.run(debug=True)
 
    
--- server/src/services.py
+++ server/src/services.py
@@ -7,7 +7,7 @@ from newsapi import NewsApiClient
 newsapi = NewsApiClient(api_key='b1d0aa01a278480a8ad685eb161edcba')
 
 def get_news(country):
-    data = newsapi.get_top_headlines(category='general', country=country, page_size=5)
+    data = newsapi.get_top_headlines(category='business', country=country, page_size=5,language='en')
     extracted_data = []
     for articles in data['articles']:
         source = articles['source']['name']
