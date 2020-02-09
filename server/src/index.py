55f70c960e198afaf6d02020ced7728c8fde74d4
wrmm1y14 <wrmm1y14@soton.ac.uk>
2020-02-09 00:42:22 +0000

    Fixed index.py

--- server/src/index.py
+++ server/src/index.py
@@ -4,23 +4,25 @@ Server File
 """
 
 from flask import Flask, render_template,request
-from services import get_news
+from services import get_news, get_fin_data
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return  render_template('TechanJS_Plot_Zooming_test.html')
+	return render_template('mainpage.html')
 
 @app.route('/info/<country>')
-def news(country):
+def get_info(country):
 	location=country
 	news_data = get_news(str(location))
 	news=[]
 	for i in news_data:
 		newsinfo={'source':i[0],'title':i[1],'link':i[2],'detail':i[3]}
 		news.append(newsinfo)
-	return render_template('news.html', post=news)
+	fin_data = get_fin_data(str(location))
+
+	return render_template('news.html', post=[news, fin_data])
 	
 if __name__ == '__main__':
 	app.run(debug=True)
--- server/src/services.py
+++ server/src/services.py
@@ -2,6 +2,8 @@
 Services File
 - Handles GET requests and Data Transformation
 """
+import os
+import pandas as pd
 from newsapi import NewsApiClient
 
 newsapi = NewsApiClient(api_key='b1d0aa01a278480a8ad685eb161edcba')
@@ -16,3 +18,20 @@ def get_news(country):
         description = articles['description']
         extracted_data.append([source, title, url, description])
     return extracted_data
+
+def get_fin_data(country):
+    path = os.getcwd()
+    df = pd.read_csv(path + f"/server/src/csv/{country}.csv", header=1)
+    df = df.rename(columns={"Unnamed: 0": "Date"})
+    date_array = df['Date'].to_list()
+    high_array = df['high'].to_list()
+    low_array = df['low'].to_list()
+    open_array = df['open'].to_list()
+    last_price_array = df['last_price'].to_list()
+    mid_average_point_array = [(val + last_price_array[idx])/2 for idx, val in enumerate(open_array)]
+    data_array = []
+    for index, value in enumerate(date_array):
+        data_array.append([value, low_array[index], open_array[index], last_price_array[index], high_array[index], mid_average_point_array[index]])
+
+    print(data_array)
+    return data_array
